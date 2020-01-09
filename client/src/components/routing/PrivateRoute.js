eea484c303b96eead7c48a5e988d27d0e9fde281
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-01-09 11:11:38 -0600

    Added private route and dashboard component

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenicated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenicated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
