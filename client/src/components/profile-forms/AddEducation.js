b46880f9a0d1df87f1c01aa897474f2a9e5bbc2c
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-01-10 18:37:48 -0600

    Added DELETE for EDU and EXP and for entire accounts

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -95,7 +95,7 @@ const AddEducation = ({ addEducation, history }) => {
                 toggleDisabled(!toDateDisabled);
               }}
             />{' '}
-            Current Job
+            Current
           </p>
         </div>
         <div class='form-group'>
